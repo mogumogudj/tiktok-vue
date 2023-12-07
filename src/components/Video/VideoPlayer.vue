5f4ab95b602b503ee548d2cde41521899abfce8f
Wolf <wolfverelst@gmail.com>
2023-12-08 00:02:45 +0100

    feat(VideoPlayer.vue): implement TikTok-style video navigation buttons

--- src/components/Video/VideoPlayer.vue
+++ src/components/Video/VideoPlayer.vue
@@ -11,6 +11,16 @@ let videos = reactive({
     data: [],
 });
 
+let currentVideoIndex = ref(0); // Track the index of the current video
+
+const switchToVideo = (index) => {
+    if (index >= 0 && index < videos.data.length) {
+        videoUrl.value = videos.data[index].video;
+        emits("update:videoDescription", videos.data[index].description);
+        currentVideoIndex.value = index;
+    }
+};
+
 onMounted (() => {
     fetch("https://api.jsonbin.io/v3/b/6548ef9954105e766fcc2c15")
     .then((response) => response.json())
@@ -28,11 +38,47 @@ onMounted (() => {
 </script>
 
 <template>
-    <div>
+    <div class="video-player">
         <video :src="videoUrl" controls autoplay loop></video>
+
+        <div class="video-buttons">
+            <button  @click="switchToVideo(currentVideoIndex - 1)" class="prev-button">Previous Video</button>
+            <button @click="switchToVideo(currentVideoIndex + 1)" class="next-button">Next Video</button>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.video-player {
+    position: relative;
+}
 
+video {
+    width: 100%;
+}
+
+.video-buttons {
+    position: absolute;
+    top: 85%;
+    transform: translateY(-50%);
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.prev-button,
+.next-button {
+    background-color: #fff;
+    border: none;
+    color: #000;
+    font-size: 24px;
+    padding: 8px;
+    cursor: pointer;
+    outline: none;
+}
+
+.prev-button:hover,
+.next-button:hover {
+    background-color: #ddd;
+}
 </style>
