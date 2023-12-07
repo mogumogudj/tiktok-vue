96319a166eb06365ed04fd7c2f35edf934f83472
Wolf <wolfverelst@gmail.com>
2023-12-07 23:53:13 +0100

    style(Chat.vue & VideoDetails.vue): tiktok style

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -3,9 +3,9 @@
     import { ref, reactive, onMounted, onBeforeMount } from 'vue';
 
     let message = ref(""); // int, string or boolean
-    let allMessages = reactive({
-        data: ["Heel", "Veel", "Berichten"],
-    }); // array or object
+    // let allMessages = reactive({
+    //     data: ["Heel", "Veel", "Berichten"],
+    // }); // array or object
 
     let messages = ref([]);
     let newComment = ref('');
@@ -41,10 +41,10 @@ onBeforeMount(() => {
 const addMessage = () => {
   if (newComment.value.trim() !== '') {
     messages.value.unshift({ user: 'You', text: newComment.value });
-    // Save user messages to local storage
-    const updatedComments = JSON.stringify(messages.value.slice(0, 10)); // Keep only the latest 10 messages
+    // save user messages to local storage
+    const updatedComments = JSON.stringify(messages.value.slice(0, 10)); // keep only the latest 10 messages
     localStorage.setItem(userCommentsKey, updatedComments);
-    newComment.value = ''; // Clear the input field after adding the message
+    newComment.value = ''; // clear the input field after adding the message
   }
 };
 
@@ -54,29 +54,24 @@ const addMessage = () => {
 
 <template>
   <div>
-    <ul>
+    <!-- <ul>
         <li v-for="message in allMessages.data">{{ message }}</li>
-    </ul>
+    </ul> -->
 
     <div>
 
         <div class="chat">
-
-          <div class="add-comment">
-      <input v-model="newComment" placeholder="Add your message" />
-      <button @click="addMessage">Submit</button>
-    </div>
-
-
-
-
     <div v-for="message in messages" :key="message._id" class="message">
       <span class="username">{{ message.user }}:</span>
       <span class="text">{{ message.text }}</span>
     </div>
-  </div>
+        </div>
 
 
+    <div class="add-comment">
+      <input v-model="newComment" placeholder="Add your message" />
+      <button @click="addMessage">Submit</button>
+    </div>
     </div>
   </div>
 </template>
@@ -88,10 +83,15 @@ const addMessage = () => {
   max-height: 300px;
   border: 1px solid #ccc;
   padding: 10px;
+  border-radius: 8px;
 }
 
 .message {
   margin-bottom: 10px;
+  padding: 8px;
+  border-radius: 8px;
+  background-color: #f8f8f8;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
 .username {
@@ -100,7 +100,27 @@ const addMessage = () => {
 }
 
 .add-comment {
+  margin-top: 10px;
   margin-bottom: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+input {
+  flex: 1;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  margin-left: 8px;
+  padding: 8px 16px;
+  background-color: #00a4d8;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 
 </style>
--- src/components/Video/VideoDetails.vue
+++ src/components/Video/VideoDetails.vue
@@ -6,11 +6,29 @@ const props = defineProps(['description']);
 
 <template>
   <div>
-    <h3>Username</h3>
-    <p>{{ description }}</p>
+    <h2 class="username">Username</h2>
+    <h3> {{ description }}</h3>
   </div>
 </template>
 
 <style scoped>
+.video-details {
+  padding: 16px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  margin-top: 16px;
+}
 
+.username {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 8px;
+}
+
+.description {
+  font-size: 16px;
+  line-height: 1.4;
+  color: #333;
+}
 </style>
