168e6e9610cd42f1996ab252c13cc0fa090379bc
Wolf <wolfverelst@gmail.com>
2023-12-07 22:31:18 +0100

    feat: Add Chat component fetching messages from external API

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -7,12 +7,25 @@
         data: ["Heel", "Veel", "Berichten"],
     }); // array or object
 
+    let messages = ref([]);
+
     // function to send a message
     const sendMessage = () => {
         allMessages.data.push(message.value);
         message.value = "";
     };
-</script>
+    
+    onMounted(async () => {
+  try {
+    const response = await fetch('https://lab5-p379.onrender.com/api/v1/messages/');
+    const data = await response.json();
+    messages.value = data;
+  } catch (error) {
+    console.error('Error fetching messages:', error);
+  }
+});
+
+    </script>
 
 <template>
   <div>
@@ -21,6 +34,15 @@
     </ul>
 
     <div>
+
+        <div class="chat">
+    <div v-for="message in messages" :key="message._id" class="message">
+      <span class="username">{{ message.user }}:</span>
+      <span class="text">{{ message.text }}</span>
+    </div>
+  </div>
+
+
         <!-- v-model is a shorthand for :value and @input event listener which allows us to update the value of the message variable -->
         <input v-model="message" type="text" placeholder="" />
         <button @click="sendMessage">Send</button>
@@ -29,5 +51,21 @@
 </template>
 
 <style scoped>
+
+.chat {
+  overflow-y: auto;
+  max-height: 300px;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.message {
+  margin-bottom: 10px;
+}
+
+.username {
+  font-weight: bold;
+  margin-right: 5px;
+}
 
 </style>
